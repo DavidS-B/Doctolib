fa7c58f608141a11bb1f7ab61041cff358e25aa5
D. S-B <dsb@mbp-de-david.lan>
2019-01-28 12:24:09 +0100

    Add availabilities array

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const app = express();
 
-const availabilities = [];
+const availabilities = [
+  {
+    date: "",
+    slots: {
+      "1000": { isAvailable: true },
+      "1030": { isAvailable: true },
+      "1100": { isAvailable: true },
+      "1130": { isAvailable: true },
+      "1400": { isAvailable: true },
+      "1430": { isAvailable: true },
+      "1500": { isAvailable: true },
+      "1530": { isAvailable: true },
+      "1600": { isAvailable: true },
+      "1630": { isAvailable: true },
+      "1700": { isAvailable: true },
+      "1730": { isAvailable: true }
+    }
+  }
+];
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+app.get("/visits", (req, res) => {
+  availabilities[0].date = req.query.date;
+  res.send(availabilities);
 });
 
 app.listen(3000, () => {
